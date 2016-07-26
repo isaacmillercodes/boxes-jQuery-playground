c835edde73f74d8c55d813d384f45d71189c78c6
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-26 15:16:07 -0600

    added links to red boxes for first problem in exercise 3

--- src/boxes.js
+++ src/boxes.js
@@ -34,6 +34,23 @@
 
 // Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
 
- $('#container div:not(.row):not(#secretBox)').css('width', '2px')
+// $('#container div:not(.row):not(#secretBox)').css('width', '2px')
+
+//Exercise 3
+
+//Add an on click handler to the container div. Console log the x and y position of the click.
+
+// $('#container').on('click', function(event) {
+//   event.preventDefault();
+//   console.log(event.pageX + ' , ' + event.pageY);
+// })
+
+//Add links inside all red box divs that take the user to galvanize. Then add an on click handler that alerts the user that you can never leave the page. Make sure the user won't leave the page after the alert!
+
+$('.boxType1').append('<a href="http://galvanize.com">Check out Galvanize!</a>');
+
+//For all box divs, add a click handler that adds an image of a cute puppy to the box. If the image is clicked again, remove the cute puppy.
+
+//Write a click handler on the container div. The click handler should turn the container background to black and the background of the original div that was clicked to white. If the user original div that was clicked happened to be the container div, change the background of the container div to lime green. You should not use any selectors for this exercise. You can do it completely with what is given to you in the event callback.
 
 }());
